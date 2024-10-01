8f54cecc2dac7e4e35be594bb731644c71758a05
quanljh <quanljh@gmail.com>
2024-10-01 16:32:05 +0800

    Fix mobile display and adjust deploy workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="quanljh">
       <HomepageHeader />
       <main></main>
     </Layout>

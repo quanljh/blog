3f60954784a06fda224cb6a88a1e715b8f330aa7
quanljh <quanljh@gmail.com>
2024-10-02 19:57:38 +0800

    Optimize UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,26 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import clsx from "clsx";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 const Header = () => {
   const { siteConfig } = useDocusaurusContext();
   const titleCharacters = siteConfig.title.split("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
 
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
         <div className={styles.headerContainer}>
           {titleCharacters.map((char, index) => (
-            <div key={index} className={styles.animate} style={{ animationDelay: `${(index + 1) * 0.3}s` }}>
+            <div
+              key={index}
+              className={isLoaded ? styles.animate : styles.animateInitial}
+              style={{ animationDelay: `${(index + 1) * 0.3}s` }}
+            >
               <svg className={styles.svg}>
                 <use href={`#Stroke-${char}`} className={styles.use} />
                 <text id={`Stroke-${char}`} className={styles.text}>
